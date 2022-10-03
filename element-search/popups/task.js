317e1de5253720ac8f14fcae77e698ff34ecfb8b
AlexanderRequiem <96475139+AlexanderRequiem@users.noreply.github.com>
2022-10-03 09:51:15 +0600

    Lecture 2, task 1

---
+++ element-search/popups/task.js
@@ -0,0 +1,23 @@
+function Run() {
+    const handlerCloseModalMain = function() {
+        elementMain.classList.remove("modal_active");
+    }
+
+    const handlerShowSuccess = function() {
+        document.getElementById('modal_success').classList.add('modal_active');
+    }
+
+
+    document.querySelectorAll('.modal__close').forEach( item => item.addEventListener('click', handlerCloseModalMain));
+    document.querySelectorAll('.show-success').forEach( 
+        item => item.addEventListener('click', handlerShowSuccess));
+
+
+    const elementMain = document.getElementById('modal_main');
+    if (elementMain !== null) {
+        elementMain.classList.add('modal_active');
+    }
+}
+
+
+Run();
